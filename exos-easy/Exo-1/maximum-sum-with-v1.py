5dd4e6b79c63edfbaf283bf6beca0fde5adabfb1
ApprendreEtEnseigner <111628262+ApprendreEtEnseigner@users.noreply.github.com>
2023-06-04 08:51:00 +0200

    faire la version 2 de l'exo two sum

--- exos-easy/Exo-1/maximum-sum-with-v1.py
+++ exos-easy/Exo-1/maximum-sum-with-v1.py
@@ -1,4 +1,12 @@
+nums = [1, 7, 5, 2]
 
-
-def sum_two(nums, target= 9):
+def two_sum(nums, target= 9):
+    dico = {}
+    for i in range(len(nums)):
+        if target - nums[i] in dico:
+            return [dico[target-nums[i]], i]
+        else:
+            dico[nums[i]] = i
+            
+print(two_sum(nums))
      
--- exos-easy/Exo-1/maximum-sum-with-v2.py
+++ exos-easy/Exo-1/maximum-sum-with-v2.py
@@ -1,21 +1,12 @@
+nums = [1, 7, 5, 2]
 
-#* Ce code définit une fonction sum_two qui prend en argument une liste de nombres nums et une valeur cible target qui par défaut est égale à 9. La fonction utilise un dictionnaire dico pour stocker les nombres de la liste nums et leur index correspondant.
-
-#* Ensuite, la fonction parcourt la liste nums à l'aide d'une boucle for, et pour chaque élément nums[i], elle vérifie si la différence entre la valeur cible target et nums[i] est présente dans le dictionnaire dico. Si c'est le cas, cela signifie que la somme de deux nombres de la liste nums est égale à la valeur cible target, et la fonction renvoie un tableau contenant les indices de ces deux nombres. Sinon, la fonction ajoute nums[i] au dictionnaire dico avec son index correspondant.
-
-#* En résumé, cette fonction cherche à trouver les indices de deux nombres dans une liste qui, lorsqu'ils sont ajoutés, donnent une valeur cible donnée.
-
-#! La complexité de ce code est linéaire, c'est-à-dire O(n), où n est la longueur de la liste nums. Cela est dû au fait que la boucle for parcourt chaque élément de la liste une seule fois, et chaque opération effectuée à l'intérieur de la boucle a une complexité constante. En utilisant un dictionnaire pour stocker les nombres de la liste et leur index correspondant, la recherche de la différence entre la valeur cible et chaque nombre se fait en temps constant, ce qui donne une complexité totale de O(n).
-nums = [1, -2, 3, 10, -4, 7, -5, 2]
-
-def sum_two(nums, target = 9):
-    dico = {}
-    for i in range(len(nums)):
-        if target - nums[i] in dico:
-    
-            return [dico[target-nums[i]], i]
+def two_sum(nums, target=9):
+    num_index = {} # dictionnaire qui associe chaque nombre à son indice
+    for index, num in enumerate(nums): # boucle qui parcourt la liste avec enumerate
+        complement = target - num # calcul du complément du nombre actuel
+        if complement in num_index: # vérification si le complément existe dans le dictionnaire
+            return [num_index[complement], index] # renvoi des indices de la paire trouvée
         else:
-            dico[nums[i]] = i
-    
+            num_index[num] = index # ajout du nombre actuel et de son indice au dictionnaire
 
-print(sum_two(nums, target = 9))
+print(two_sum(nums))
